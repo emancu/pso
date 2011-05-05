b216a53b017f2dbd5b603d5c4d7888ad1aa341b1
March <m2.march@gmail.com>
2011-05-05 16:49:00 -0300

    Implementation of semaphore methods. It cannot be tested since it needs loader implementation.

--- tp1/src/include/sem.h
+++ tp1/src/include/sem.h
@@ -2,7 +2,9 @@
 #define __SEM_H__
 
 #include <tipos.h>
+#include <loader.h>
 
+/* ¿¿?? */
 typedef sint_32 sem_id;
 
 typedef struct str_sem_t {
@@ -10,6 +12,10 @@ typedef struct str_sem_t {
 	uint_32 q; /* Wait-event queue */
 } sem_t;
 #define SEM_NEW(val) (sem_t){.vl = (val), .q = (uint_32)-1}
+
+/* Esta función inicializa el arreglo de semáforos con semáforos vacíos.
+ * Distinguimos un semáforo vacío como aquel cuyo valor es -1 */
+void sem_init();
 
 void sem_wait(sem_t* s);
 void sem_signaln(sem_t* s);
