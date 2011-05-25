2c82170cf1e2b93df14ad66e3ed7fa6ecd3b3718
M2March <m2.march@gmail.com>
2011-05-25 20:12:57 -0300

    Trying to compile with symbols

--- tp1/src/kernel/kernel.c
+++ tp1/src/kernel/kernel.c
@@ -20,13 +20,21 @@
 extern void* _end;
 extern pso_file task_task1_pso;
 extern pso_file task_task2_pso;
+extern char* kernel_sym;
 
 /* Entry-point del modo protegido luego de cargar los registros de
  * segmento y armar un stack */
 void kernel_init(void) {
+  int i;
   gdt_init();
   clear_screen();
 
+  char* kernel_sym_r = (char*)&kernel_sym;
+  printf("Kernel_sym: %x\n", &kernel_sym);
+  for (i = 0; i < 100; i++)
+    vga_printf(3+(i/vga_cols), i%vga_cols, "%c", VGA_BC_BLACK | VGA_FC_WHITE, kernel_sym_r[i]);
+
+  breakpoint();
   //init all modules
   idt_init();
   debug_init();
