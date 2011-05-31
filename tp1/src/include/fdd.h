5fa65988f16c312ef37318a45c58188ca65baf52
M2March <m2.march@gmail.com>
2011-05-31 20:43:05 -0300

    Added functions for specify, command, reset, dumpreg, lock, and more. None of them where really tested

--- tp1/src/include/fdd.h
+++ tp1/src/include/fdd.h
@@ -11,7 +11,10 @@
  * de los comandos del fdc. */
 typedef struct str_fdc_stat {
   uint_8 st0, st1, st2, c, h, r, n;
-  uint_8 dor, stA, stB, dsr, dir, ccr;
+  uint_8 dor, srA, srB, dsr, dir, ccr, msr;
+  uint_8 config1, config2; //Mantienen los últimos valores de configuración usados, también los completa dumpreg.
+  uint_8 pcn0, pcn1, pcn2, pcn3; //Present cilinder number (dumpreg)
+  uint_8 srt_hut, hlt_nd, sc_eot, perpendicular; ///(dumpreg)
 } __attribute__((packed)) fdc_stat;
 
 blockdev* fdd_open(int nro);
@@ -49,8 +52,56 @@ char fdd_dma_st();
  * Puede fallar por timeout a la hora de enviar comandos. 
  * 'eis', 'efifo' y 'poll' activan o desactivan bits en el commando de parámetros.
  * 'fifothr' es el threshold de la fifo, debe ir entre 0 y 15. 'pretrk' es la
- * precompensación de track y varía entre 0x00 y 0xFF */
+ * precompensación de track y varía entre 0x00 y 0xFF.
+ * El significado del bit 'poll' está invertido. poll == 1 activa polling y poll == 0 lo desactiva.
+ * Lo mismo pasa con el bit 'efifo'. Para el fdc, 'efifo' == 1 significa deshabilitar fifo,
+ * para la función 'efifo' == 1 significa habilitar fifo. 
+ * Devuelve 0 en caso de éxito, sino devuelve un código de error negativo. */
 int fdd_configure(fdc_stat* fdc, char eis, char efifo, char poll, char fifothr, char pretrk);
+
+/* Esta función ejecuta el comando de especificación del fdc.
+ * De 'srt' y 'hut' se utiliza solo el primer nibble,
+ * de 'hlt' se utilizan solo los primeros 7 bits. 
+ * Devuelve 0 en caso de éxito, sino devuelve un código de error negativo. */
+int fdd_specify(fdc_stat* fdc, char srt, char hut, char hlt, char nd);
+
+/* Esta función ejecuta el comando lock estableciendo
+ * el valor según 'lock'. Además verifica que el byte
+ * de retorno concuerde con el lock enviado. 
+ * En caso de timeout o discordancia se devuelve un error
+ * sino se devuelve 0. */
+int fdd_lock(fdc_stat* fdc, char lock);
+
+/* Este función ejecuta el comando 'dumpreg' en el fdc y completa 
+ * la estructura con la información recibida. */
+int fdd_dumpreg(fdc_stat* fdc);
+
+/* Esta función actualiza el valor de los registros de lectura en 'fdc' */
+void fdd_update_R_regs(fdc_stat* fdc);
+
+/* Esta función actualiza el estado de 'fdc'. (Llama a dumpreg y update_R)*/
+int fdd_update_status(fdc_stat* fdc);
+
+/* Esta función actualiza el estado de 'fdc' y luego imprime su información. */
+void fdd_print_status(fdc_stat* fdc);
+
+/* Esta función ejecuta un reset completo del fdc. Es utilizado para la primera
+ * inicialización del mismo. Activa implied seek, dma, fifo, fifo. 
+ * Threshold toma valor 8 y precomp toma valor 0 (DEFAULT)
+ * (redomendado en: http://wiki.osdev.org/Floppy_Disk_Controller#Configure) 
+ * Se establecen valores para srt, hlt y hut. Estos valores son específicos
+ * del hw en cuestión, por lo que se utilizan valores seguros
+ * (http://wiki.osdev.org/Floppy_Disk_Controller#SRT.2C_HLT_and_HUT). 
+ * La función también establece valores para DSR y CCR. El segundo prácticamente
+ * no tiene utilidad y refleja el estado de DSR, por lo que se utiliza únicamente este.
+ * El valor de CCR se mantiene al igual que luego del reset.
+ * En DSR se matiene el valor del DATA Rate y Precompensation.
+ * Estos valores se establecen en 0 por defecto siendo los valores más razonables.
+ * (Ver http://wiki.osdev.org/Floppy_Disk_Controller#CCR_and_DSR). 
+ * DOR se establece en 0x40. Esto apaga los motores y deshabilita las interrupciones,
+ * se pretende evitar la IRQ6 que sucede luego del reset.
+ * (Ver http://wiki.osdev.org/Floppy_Disk_Controller#Controller_Reset) */
+int fdd_full_reset(fdc_stat* fdc);
 
 void fdd_init(void);
 #endif
--- tp1/src/kernel/fdd.c
+++ tp1/src/kernel/fdd.c
@@ -57,6 +57,7 @@
 #define FDD_CMD_SCAN_EQUAL     0x11
 #define FDD_CMD_PERPENDICULAR_MODE 0x12
 #define FDD_CMD_CONFIGURE      0x13
+#define FDD_CMD_LOCK 0x14
 #define FDD_CMD_VERIFY         0x16
 #define FDD_CMD_SCAN_LOW_OR_EQUAL 0x19
 #define FDD_CMD_SCAN_HIGH_OR_EQUAL 0x1D
@@ -80,6 +81,7 @@
 /* Error codes */
 #define FDD_ERROR_TIMEOUT -2
 #define FDD_ERROR_VERSION_NOT_CORRECT -3
+#define FDD_ERROR_LOCKCMD -4
 
 /* Device variables */
 
@@ -103,6 +105,9 @@ blockdev* fdd_open(int nro) {
 	return NULL;
 }
 
+/**************************************************/
+/* Funciones generales de comunicación con el fdc */
+/**************************************************/
 
 int fdd_send_byte(uint_8 byte) {
 	uint_32 timeout = DEFAULT_TIMEOUT;
@@ -134,6 +139,9 @@ uint_8 fdd_get_byte(int* stat) {
   return inb(FDD_PORT+PORT_DATA);
 }
 
+/*********************************/
+/* Funciones de comandos del fdc */
+/*********************************/
 
 int fdd_version() {
   int st;
@@ -171,10 +179,158 @@ char get_dma_st() {
   return (msr & 0x20)? 0 : 1;
 }
 
+
+int fdd_configure(fdc_stat* fdc, char eis, char efifo, char poll, char fifothr, char pretrk) {
+  int st;
+  char param1 = 0x0; //Estas variables tendran los parámetros del comando configure
+  param1 |= ((eis > 0) << 6); //Preparo param1 con los valores de las variables
+  param1 |= ((efifo <= 0) << 5); //Si me llega <= 0 significa deshabilitá polling, entonces pongo un 1
+  param1 |= ((poll <= 0) << 4); //Si me llega <= 0 signifca deshabilitá polling, entonces pongo un 1
+  param1 |= (fifothr & 0xF);
+
+  st = fdd_send_byte(FDD_CMD_CONFIGURE); //Envío el comando configure
+  if (st < 0)
+    return st;
+  st = fdd_send_byte(0x00); //El comando continúa con un 0
+  if (st < 0)
+    return st;
+  st = fdd_send_byte(param1); //Envío el primer parámetro
+  if (st < 0)
+    return st;
+  fdc->config1 = param1;
+  st = fdd_send_byte(pretrk); //Envío el primer parámetro
+  if (st < 0)
+    return st;
+  fdc->config2 = pretrk;
+  return 0;
+}
+
+int fdd_specify(fdc_stat* fdc, char srt, char hut, char hlt, char nd) {
+  char param1 = 0x0, param2 = 0x0;
+  int st;
+  param1 = ((srt & 0x0F) << 4) | (hut & 0x0F);
+  param2 = ((hlt & 0x7F) << 1) | (nd > 0);
+  st = fdd_send_byte(FDD_CMD_SPECIFY);
+    if (st < 0) return st;
+  st = fdd_send_byte(param1);
+    if (st < 0) return st;
+  st = fdd_send_byte(param2);
+    if (st < 0) return st;
+  fdc->srt_hut = param1;
+  fdc->hlt_nd = param2;
+  return 0;
+}
+
+int fdd_dumpreg(fdc_stat* fdc) {
+  int st;
+  st = fdd_send_byte(FDD_CMD_DUMPREG);
+    if (st < 0) return st;
+  fdc->pcn0 = fdd_get_byte(&st);
+    if (st < 0) return st;
+  fdc->pcn1 = fdd_get_byte(&st);
+    if (st < 0) return st;
+  fdc->pcn2 = fdd_get_byte(&st);
+    if (st < 0) return st;
+  fdc->pcn3 = fdd_get_byte(&st);
+    if (st < 0) return st;
+  fdc->srt_hut = fdd_get_byte(&st);
+    if (st < 0) return st;
+  fdc->hlt_nd = fdd_get_byte(&st);
+    if (st < 0) return st;
+  fdc->sc_eot = fdd_get_byte(&st);
+    if (st < 0) return st;
+  fdd_get_byte(&st);
+    if (st < 0) return st;
+  fdc->config1 = fdd_get_byte(&st);
+    if (st < 0) return st;
+  fdc->config2 = fdd_get_byte(&st);
+    if (st < 0) return st;
+
+  return 0;
+}
+
+int fdc_lock(fdc_stat* fdc, char lock) {
+  int st, ret;
+  st = fdd_send_byte(FDD_CMD_LOCK | ((lock > 0) << 7));
+  if (st < 0)
+    return st;
+  ret = fdd_get_byte(&st);
+  if (st < 0)
+    return st;
+  if (ret != (0x0 | ((lock > 0) << 4)))
+    return FDD_ERROR_LOCKCMD;
+  return 0;
+}
+
+
+/****************************/
+/* Lectura de stado del fdc */
+/****************************/
+
+void fdd_update_R_regs(fdc_stat* fdc) {
+  fdc->srA = inb(FDD_PORT+PORT_SRA);
+  fdc->srB = inb(FDD_PORT+PORT_SRB);
+  fdc->msr = inb(FDD_PORT+PORT_MSR);
+  fdc->dir = inb(FDD_PORT+PORT_DIR);
+}
+
+int fdd_update_status(fdc_stat* fdc) {
+  fdd_update_R_regs(fdc);
+  int st = fdd_dumpreg(fdc);
+  if (st < 0) return st;
+  return 0;
+}
+
+void fdd_print_status(fdc_stat* fdc) {
+  int st = fdd_update_status(fdc);
+  if (st < 0) {
+    printf("! FDC: Error en fdd_update_status.");
+    return;
+  }
+  printf("Fdd internal status: ");
+  printf(" st0 %x | st1 %x | c %x | h %x | r %x | n %x", fdc->st0, fdc->st1, fdc->c, fdc->h, fdc->r, fdc->n);
+  printf(" dor %x | srA %x | srB %x | dsr %x | dir %x | ccr %x | msr %x", fdc->dor, fdc->srA, fdc->srB, fdc->dsr, fdc->dir, fdc->ccr, fdc->msr);
+  printf(" config1 %x | config2 %x", fdc->config1, fdc->config2);
+  printf(" pcn0 %x | pcn1 %x | pcn2 %x | pcn3 %x", fdc->pcn0, fdc->pcn1, fdc->pcn2, fdc->pcn3);
+  printf(" str_hut %x | hlt_nd %x | sc_eot %x | perp %x", fdc->srt_hut, fdc->hlt_nd, fdc->sc_eot, fdc->perpendicular);
+}
+
+int fdd_full_reset(fdc_stat* fdc) {
+  //Establezco DOR en 0x40 para dehabilitar los motores y las interrupciones.
+  //De otra forma el reset provocará un IRQ6 (la datasheet no dice nada, pero esto aparece en wiki osdev)
+  outb(FDD_PORT+PORT_DOR, 0x40);
+  //Establezco DSR en 0x80, provocando un reset
+  outb(FDD_PORT+PORT_DSR, 0x80);
+  //Establezco el CCR en 0
+  outb(FDD_PORT+PORT_CCR, 0x0);
+  fdc->ccr = 0x0;
+  fdc->dsr = 0x0; //El bit 8 se desactiva luego del reset
+  fdc->dor = 0x40;
+
+  int st;
+  //Senseo los drives
+
+  //Configuro el fdc con *configure*
+  st = fdd_configure(fdc, 1, 1, 0, 8, 0x0);
+  if (st < 0)
+    return st;
+  //Especifico el fdc con *specify*
+  st = fdd_specify(fdc, 8, 5, 15, 0);
+  if (st < 0)
+    return st;
+  //Recalibro los drives
+  //Apago todos los motores
+  //Eligo el drive 0
+  return 0;
+}
+
 /** Init **/
 
 void fdd_init(void) {
+  int st;
 	//Chequeo la versión
+  printf("----FDC Initialization ----");
+  printf("FDC: Checking fdc version");
   uint_8 ver = fdd_version();
   if (ver != 0x90) {
     if (ver == FDD_ERROR_TIMEOUT) {
@@ -187,9 +343,11 @@ void fdd_init(void) {
 
   // printf("1 << 0 (%x) | 1 << 1 (%x) | 1 << 2 (%x) | 1 << 3 (%x) | 1 << 4 (%x)", 1 << 0, 1 << 1, 1 << 2, 1 << 3, 1 << 4);
 
-  //Establezco el estado del fdc
-  fdc.dor = 0x04;
-  outb(FDD_PORT+PORT_DOR, fdc.dor); //Actualizo el fdc con mi dor actual
+  printf("FDC: Reseting fdc...");
+  st = fdd_full_reset(&fdc);
+  printf("FDC: Reset successful.");
+
+  fdd_print_status(&fdc);
 
   //Activo el modo DMA
   printf("FDC: Is dma on? %d", get_dma_st());
